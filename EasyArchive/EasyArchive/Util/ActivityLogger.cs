c5fb81ed9961b87a11c5ef2b4979c55d9e8d2ea2
rtomon <rtomon@softsystem.pl>
2014-10-28 19:34:18 +0100

    init project

---
+++ EasyArchive/EasyArchive/Util/ActivityLogger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyArchive.Util
+{
+    class ActivityLogger
+    {
+        ITextDisplay _display;
+
+        public ActivityLogger(ITextDisplay display)
+        {
+            _display = display;
+        }
+
+        public void Show(string msg)
+        {
+            _display.Show(msg);
+        }
+    }
+}
---
+++ EasyArchive/EasyArchive/Util/LabelDisplay.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EasyArchive.Util
+{
+    class LabelDisplay : ITextDisplay
+    {
+        Label _label;
+
+        public LabelDisplay(Label label)
+        {
+            _label = label;
+        }
+
+        public void Show(string message)
+        {
+            _label.Text = message;
+        }
+    }
+}
